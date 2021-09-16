cb3bb0cd8d2d8158da2870190e8744b772ac97e1
GMShimon <simon.rosedale99@gmail.com>
2021-09-16 12:42:49 +0600

    complete

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 //API key
-const APIkey = 'a28951ae49a74c7cb3411354211509';
+const APIkey = '4505adf24099746b640663d26b01a045';
 
 /* getting today's date */
 const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
@@ -12,72 +12,81 @@ const day = dayNames[d.getDay()];
 const year = d.getFullYear();
 
 const second = d.getSeconds(); // => 51
-const hour = d.getHours(); // => 9
+let hour // => 9
 let minute;
 
 let Typography;
-if (hour < 12) {
+if (d.getHours() < 12) {
     Typography = 'AM';
 }
 else {
     Typography = 'PM'
 }
+//minute check
 if (d.getMinutes() < 10) {
     minute = '0' + d.getMinutes();
 }
 else {
     minute = d.getMinutes();
 }
+//hour check
+if (d.getHours() > 12) {
+    hour = d.getHours() - 12;
+}
+else {
+    hour = d.getHours();
+}
+const timeP = document.getElementById('time');
+timeP.innerText = `${hour}:${minute}${Typography}`
 /* End */
 
+document.getElementById('input-field').addEventListener('keypress', function (e) {
+    if (e.key == 'Enter') {
+        loadData();
+    }
+})
+
+//toggle spinners
+const toggleSpinner = isBlock => {
+    document.getElementById('spinner').style.visibility = isBlock;
+}
 
 const loadData = () => {
     const inputField = document.getElementById('input-field');
-    const inputValue = inputField.value;
+    const inputText = inputField.value;
     inputField.value = '';
-
-    //making spinner visible
-
-    //making details section clear
-    const detailsContainer = document.getElementById('details');
-    detailsContainer.textContent = '';
-
-    console.log(inputValue);
-    if (inputValue != '') {
-        //fetching data
-        fetch(`http://api.weatherapi.com/v1/current.json?key=a28951ae49a74c7cb3411354211509&q=${inputValue}&aqi=no`)
-            .then(response => response.json())
-            .then(data => displayData(data));
-        spinnerDisplay('block');
-    }
-    else {
-        alert('Please give city name');
-    }
-};
-// display the details of the city
+    toggleSpinner('visible');
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputText}&appid=${APIkey}&units=metric`;
+    fetch(url)
+        .then(response => response.json())
+        .then(data => displayData(data))
+}
+const seText = (id, text) => {
+    document.getElementById(id).innerText = text;
+}
 const displayData = (data) => {
-    const detailsContainer = document.getElementById('details');
-    detailsContainer.textContent = '';
-    const div = document.createElement('div');
-    div.innerHTML = `
-        <h2 class="text-center">weather details</h2>
-        <h2 class="text-white text-center">${data.location.name}</h2>
-        <span class="fs-4 ms-5 fw-normal text-center text-Secondary">(${day}/${month}/${year}) ${hour}:${minute}${Typography}</span>
-        <div class="d-flex justify-content-center align-items-center">
-            <img src="${data.current.condition.icon}" alt="">
-            <h4 class="text-white text-center">${data.current.condition.text}</h4>
-        </div>
-        <h4 class="text-center text-white">Temperature: ${data.current.feelslike_c} °C</h4>
-        <div class="top-corner">
-            <p class="fw-bold"></p>
-        </div>
+    //getting the url of weather icon
+    const imageUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+    document.getElementById('image').setAttribute('src', imageUrl);
+    //setting the text 
+    seText('city', data.name);
+    seText('description', data.weather[0].description);
+    document.getElementById('temperature').innerText = `
+        ${data.main.temp} °C
     `;
-    detailsContainer.appendChild(div);
-    spinnerDisplay('none')
-};
-const spinnerDisplay = (style) => {
-    document.getElementById('spinner').style.display = style;
-};
-
-
+    toggleSpinner('hidden');
+    //fetching the time of the current city
+    const timeUrl = `https://timezone.abstractapi.com/v1/current_time/?api_key=4c85885a368445cbbff2408cd480de7a&location=${data.name},${data.sys.country}`
+    fetch(timeUrl)
+        .then(response => response.json())
+        .then(time => setTime(time))
+}
+const setTime = (time) => {
+    console.log(time)
+    const x = time.datetime.split(' ');
+    console.log(x);
+    document.getElementById('time-city').innerText = `
+    time:${x[1]} // ${day}
+    `;
+}
 
